24b01362ea31a9d9cc54fd3331be0e07968ed5ba
luyihan1995 <ary60201@gmail.com>
2024-02-29 21:30:22 +0800

    Crate label

--- N09310011/N09310011/Form1.cs
+++ N09310011/N09310011/Form1.cs
@@ -16,5 +16,15 @@ namespace N09310011
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
